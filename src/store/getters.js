b0edda20551ce6196e0892e4ea2216ec0a770667
HeanBro <07andi20030720@gmail.com>
2024-11-29 20:55:51 +0800

    顶部头像展示

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,8 @@
 const getters = {
   roles: state => state.user.roles,
   sidebarRouters: state => state.permissions.sidebarRouters,
-  sidebar: state => state.app.sidebar
+  sidebar: state => state.app.sidebar,
+  avatar: state => state.user.avatar
 }
 
 export default getters
