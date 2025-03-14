24740444fbbeed262091bbbf8db530f634d6bd4a
HeanBro <07andi20030720@gmail.com>
2025-03-14 21:40:22 +0800

    角色操作按钮样式

---
+++ src/components/RightToolbar/index.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'RightToolbar'
+}
+</script>
+
+<template>
+  <div class="top-right-btn">
+    <el-row>
+      <el-tooltip content="显示搜索" placement="top">
+        <el-button size="mini" circle icon="el-icon-search"></el-button>
+      </el-tooltip>
+      <el-tooltip content="刷新" placement="top">
+        <el-button size="mini" circle icon="el-icon-refresh"></el-button>
+      </el-tooltip>
+      <el-tooltip content="显隐列" placement="top">
+        <el-button size="mini" circle icon="el-icon-menu"></el-button>
+      </el-tooltip>
+    </el-row>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
