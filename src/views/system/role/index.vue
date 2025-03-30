5aa2110f66e307c016543f68ceff37515c950c49
HeanBro <07andi20030720@gmail.com>
2025-03-31 02:09:50 +0800

    杂项（不重要）

--- src/views/system/role/index.vue
+++ src/views/system/role/index.vue
@@ -47,39 +47,39 @@ export default {
     <el-form :inline="true" ref="queryForm" :model="queryParams">
       <el-form-item label="角色名称" prop="roleName">
         <el-input
-          v-model="queryParams.roleName"
-          placeholder="请输入角色名称"
-          size="small"
-          style="width: 240px"
+            v-model="queryParams.roleName"
+            placeholder="请输入角色名称"
+            size="small"
+            style="width: 240px"
         />
       </el-form-item>
       <el-form-item label="权限字符" prop="roleKey">
         <el-input
-          v-model="queryParams.roleKey"
-          placeholder="请输入权限字符"
-          size="small"
-          style="width: 240px"
+            v-model="queryParams.roleKey"
+            placeholder="请输入权限字符"
+            size="small"
+            style="width: 240px"
         />
       </el-form-item>
       <el-form-item label="状态" prop="status">
         <el-select
-          v-model="queryParams.status"
-        placeholder="角色状态"
-        size="small"
-        style="width: 240px"
+            v-model="queryParams.status"
+            placeholder="角色状态"
+            size="small"
+            style="width: 240px"
         >
         </el-select>
       </el-form-item>
       <el-form-item label="创建时间">
-      <el-date-picker
-          v-model="dateRange"
-          size="small"
-          style="width: 240px"
-          type="daterange"
-          range-separator="-"
-          start-placeholder="开始时间"
-          end-placeholder="结束时间"
+        <el-date-picker
+            v-model="dateRange"
+            size="small"
+            style="width: 240px"
+            type="daterange"
+            range-separator="-"
             value-format="yyyy-MM-dd"
+            start-placeholder="开始时间"
+            end-placeholder="结束时间"
         >
         </el-date-picker>
       </el-form-item>
@@ -92,34 +92,34 @@ export default {
     <el-row :gutter="10">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          size="mini"
-          icon="el-icon-plus"
-          plain
+            type="primary"
+            size="mini"
+            icon="el-icon-plus"
+            plain
         >新增</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="success"
-          size="mini"
-          icon="el-icon-edit"
-          plain
+            type="success"
+            size="mini"
+            icon="el-icon-edit"
+            plain
         >修改</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="danger"
-          size="mini"
-          icon="el-icon-delete"
-          plain
+            type="danger"
+            size="mini"
+            icon="el-icon-delete"
+            plain
         >删除</el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
-          type="warning"
-          size="mini"
-          icon="el-icon-download"
-          plain
+            type="warning"
+            size="mini"
+            icon="el-icon-download"
+            plain
         >导出</el-button>
       </el-col>
 
@@ -135,9 +135,9 @@ export default {
       <el-table-column label="状态" width="100" prop="status">
         <template v-slot="scope">
           <el-switch
-            v-model="scope.row.status"
-            active-value="0"
-            inactive-value="1"
+              v-model="scope.row.status"
+              active-value="0"
+              inactive-value="1"
           >
 
           </el-switch>
@@ -147,24 +147,24 @@ export default {
       <el-table-column label="操作" align="center">
         <template v-slot="scope">
           <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
+              size="mini"
+              type="text"
+              icon="el-icon-edit"
           >
             修改
           </el-button>
           <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-delete"
+              size="mini"
+              type="text"
+              icon="el-icon-delete"
           >
             删除
           </el-button>
           <el-dropdown
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
+              size="mini"
+              type="text"
+              icon="el-icon-edit"
+              @click="handleUpdate(scope.row)"
           >
             <span class="el-dropdown-link">
               <i class="el-icon-d-arrow-right"></i>&nbsp;更多
@@ -178,11 +178,11 @@ export default {
       </el-table-column>
     </el-table>
     <pagination
-      :total="total"
-      :page-sizes="[1,2,5,10]"
-      :page.sync="queryParams.pageNum"
-      :limit.sync="queryParams.pageSize"
-      @pagination="getList"
+        :total="total"
+        :page-sizes="[1,2,5,10]"
+        :page.sync="queryParams.pageNum"
+        :limit.sync="queryParams.pageSize"
+        @pagination="getList"
     >
 
     </pagination>
